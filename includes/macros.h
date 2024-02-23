693e1e4eddd17c25f4cb0203dcdb15bbc3ccb5e2
me3za <medkrix1@gmail.com>
2024-02-23 21:22:46 +0100

    united macros

---
+++ includes/macros.h
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   macros.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: me3za <me3za@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/24 10:04:26 by echoukri          #+#    #+#             */
+/*   Updated: 2024/02/23 21:20:54 by me3za            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MACROS_H
+#define MACROS_H
+
+#define WIDTH 1400
+#define HEIGHT 1200
+#define LOADING_MAP_MESSAGE "Reading the map...\n"
+#define SUCCESSFUL_READ_MESSAGE "\033[0;32mSuccessfully read the map\n\033[0;0m"
+#define BASE_COLOR
+
+// mouse events
+#define ZOOM_IN 4
+#define ZOOM_OUT 5
+
+// macos keyboard events
+// #define KEY_LEFT 123
+// #define KEY_RIGHT 124
+// #define KEY_DOWN 125
+// #define KEY_UP 126
+// #define KEY_W 13
+// #define KEY_D 2
+// #define KEY_S 1
+// #define KEY_A 0
+// #define KEY_ESC 53
+// #define KEY_ETR 36
+// #define KEY_T 17
+// #define KEY_Y 16
+// ubuntu keyboard events
+#define KEY_UP 65362
+#define KEY_RIGHT 65363
+#define KEY_DOWN 65364
+#define KEY_LEFT 65361
+#define KEY_W 122
+#define KEY_D 100
+#define KEY_S 115
+#define KEY_A 113
+#define KEY_ESC 65307
+#define KEY_ETR 65293
+#define KEY_T 116
+#define KEY_Y 121
+
+#endif
